201f8a756608d4fc60f6b8f4807bdcfcd898b1f7
unknown <tarik.pasalic777@gmail.com>
2021-05-21 17:04:16 +0200

    css1

--- src/App.js
+++ src/App.js
@@ -65,18 +65,32 @@ const correctPinNumber="1234";
     <div className="App">
     <div className="container">
     <div className="screen">{code}</div>
+    <div class="row">
     <button onClick={add} disabled={blocked?true:false} value="1">1</button>
     <button onClick={add} disabled={blocked?true:false}  value="2">2</button>
     <button onClick={add} disabled={blocked?true:false}  value="3">3</button>
+
+    </div>
+    <div class="row">
+   
     <button onClick={add} disabled={blocked?true:false}   value="4">4</button>
     <button onClick={add} disabled={blocked?true:false}  value="5">5</button>
     <button onClick={add} disabled={blocked?true:false}  value="6">6</button>
+    </div>
+    <div class="row">
     <button onClick={add} disabled={blocked?true:false}  value="7">7</button>
     <button onClick={add} disabled={blocked?true:false}  value="8">8</button>
     <button onClick={add} disabled={blocked?true:false}  value="9">9</button>
-    <button onClick={clear}  value="clear">clear</button>
+
+    </div>
+   <div className="row">
+
+   <button onClick={clear}  value="clear">clear</button>
     <button onClick={add} disabled={blocked?true:false}  value="0">0</button>
     <button onClick={enter} value="enter">enter</button>
+   </div>
+    
+   
 
     </div>
     </div>
